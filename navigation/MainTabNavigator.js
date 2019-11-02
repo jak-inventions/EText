ee6badf60819bf75b4ac22a0e706bbd4c4a0cddd
Jak <jakinventions@gmail.com>
2019-11-02 14:50:36 -0400

    First

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import ConversationsScreen from '../screens/ConversationsScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import ConversationScreen from '../screens/ConversationScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  config
+);
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+HomeStack.path = '';
+
+const conversationsStack = createStackNavigator(
+  {
+    Links: ConversationsScreen,
+  },
+  config
+);
+
+conversationsStack.navigationOptions = {
+  tabBarLabel: 'Conversations',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-text' : 'md-text'} />
+  ),
+};
+
+conversationsStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+  },
+  config
+);
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  ),
+};
+
+SettingsStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
+  HomeStack,
+  conversationsStack,
+  SettingsStack,
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
---
+++ screens/ConversationsScreen.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Text, ScrollView, StyleSheet } from 'react-native';
+import Touchable from 'react-native-platform-touchable';
+
+export default function ConversationsScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <Contact name="Keanu Reeves" email="example@example.com" />
+      <Contact name="Bob Ross" email="example@example.com" />
+      <Contact name="Elon Musk" email="example@example.com" />
+      <Contact name="Robby Rotten" email="example@example.com" />
+    </ScrollView>
+  );
+}
+
+class Contact extends React.Component{
+
+  render(){
+    return (
+      <Touchable style={styles.contact} background={Touchable.Ripple('grey')}>
+        <Text>
+          <Text style={styles.contactHeader}>{this.props.name + "\n"}</Text>
+          <Text style={styles.contactText}>{this.props.email}</Text>
+        </Text>
+      </Touchable>
+    );
+  };
+
+}
+
+ConversationsScreen.navigationOptions = {
+  title: 'Conversations',
+  headerTitleStyle: {
+    fontWeight: 'normal',
+  },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 0,
+    backgroundColor: '#fff',
+  },
+  contact: {
+    backgroundColor: '#fdfdfd',
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  contactHeader: {
+    fontSize: 15,
+  },
+  contactText: {
+    fontSize: 13,
+    marginLeft: 5,
+  }
+});
