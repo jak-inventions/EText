afff4da0c270bbc765972c94dde48355717c6c20
Jak <jaksawesome10@github.com>
2020-03-03 13:58:56 -0500

    small styling things

--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,24 @@ var socket = io.connect('https://etext-messaging.herokuapp.com');
 
 // submit text message without reload/refresh the page
 $('form').submit(function(e){
-    e.preventDefault(); // prevents page reloading
-    socket.emit('chat_message', $('#txt').val());
-    $('#txt').val('');
-    return false;
+    e.preventDefault();
+    if($('#txt').val() != ""){
+      socket.emit('chat_message', $('#txt').val());
+      $('#txt').val('');
+      return false;
+    }
 });
 
 // append the chat text message
 socket.on('chat_message', function(msg){
     $('#messages').append($('<li>').html(msg));
+    $('#messages').append($('<br/>'));
 });
 
 // append text if someone is online
 socket.on('is_online', function(username) {
     $('#messages').append($('<li>').html(username));
+    $('#messages').append($('<br/>'));
 });
 
 // ask username
