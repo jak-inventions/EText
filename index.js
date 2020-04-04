46d6c60c612f2caea200935605608a966ea758bc
Jak <jaksawesome10@gmail.com>
2020-04-04 17:12:09 -0400

    stuff

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 
 app.listen(port, function(){
   console.log('Running on port ' + port);
 });
 
 app.get('/', function(req, res){
-  res.render('index.pug', {
+  res.render('index', {
     signedIn: false
   });
 });
 
 app.get('/messaging', function(req, res){
-  
+  res.send('Messaging');
+});
+
+app.get('/platforms', function(req, res){
+  res.send('Platforms');
+});
+
+app.get('/signIn', function(req, res){
+  res.render('signIn');
 });
