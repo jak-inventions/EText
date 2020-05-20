73e0d195b323197cdbb2dba0a0d70b2c2ddc3963
Jak <jaksawesome10@gmail.com>
2020-05-20 19:21:25 -0400

    more stuff

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+// Imports
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// Router Imports
+const uiRouter = require('./routers/ui.js');
+const authRouter = require('./routers/auth.js');
+
+//Constants
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Settings
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Middleware
+app.use(express.static('public'));
+
+// Routers
+app.use(uiRouter);
+app.use(authRouter);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
