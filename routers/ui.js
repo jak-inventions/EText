fcea27a507ac771d68f8b6813ccf654b4c254b20
Jak <jaksawesome10@gmail.com>
2020-05-22 19:46:48 -0400

    stuff

--- routers/ui.js
+++ routers/ui.js
@@ -9,4 +9,19 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
+router.get('/forgotPassword', (req, res) => {
+  // ToDo
+  res.render('forgotPassword');
+});
+
+router.get('/reset/:token', function(req, res) {
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+    if (!user) {
+      console.log('Password reset token is invalid or has expired.');
+      return res.redirect('/');
+    }
+    res.render('reset', {token: req.params.token});
+  });
+});
+
 module.exports = router;
