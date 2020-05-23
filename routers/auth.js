a1f31f46fa4dd57857c4035712e3b0c7a07675dd
Jak <jaksawesome10@gmail.com>
2020-05-23 15:36:56 -0400

    sign in bug fix

--- routers/auth.js
+++ routers/auth.js
@@ -67,7 +67,7 @@ router.post('/login', async (req, res) => {
 
   // Create and assign a token
   const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-  res.cookie('auth-token', token).redirect('/message');
+  res.cookie('auth-token', token).redirect('/messaging');
   //res.header('auth-token', token).send(token);
 });
 
