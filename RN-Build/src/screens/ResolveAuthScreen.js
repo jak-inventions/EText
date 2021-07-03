ae3989b663fb36eb42f40b08f9a5974310544f85
John Khachian <jaksawesome10@gmail.com>
2021-07-03 11:47:09 -0400

    loading animation

--- RN-Build/src/screens/ResolveAuthScreen.js
+++ RN-Build/src/screens/ResolveAuthScreen.js
@@ -1,5 +1,6 @@
 // Check to see if user is signed in, & direct accordingly
 import React, { useContext } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
 
 const ResolveAuthScreen = ({ navigation }) => {
@@ -7,9 +8,19 @@ const ResolveAuthScreen = ({ navigation }) => {
     checkSignedIn();
 
     return (
-        <></>
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color="#0000ff" />
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
 
 export default ResolveAuthScreen;
 
